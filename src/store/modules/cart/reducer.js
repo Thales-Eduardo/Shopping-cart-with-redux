fddd56505f2a48d9cee1e5a024fc5821c9ca3902
Thales Eduardo <thalesdev22@gmail.com>
2021-08-24 13:15:00 -0300

    Produtos duplicados.

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,14 +1,23 @@
-export function cart(state, action) {
+import produce from 'immer';
+
+export function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [
-        ...state,
-        {
-          ...action.products,
-          amount: 1,
-        },
-      ];
+      return produce(state, (draft) => {
+        const productIndex = draft.findIndex(
+          (p) => p.id === action.products.id,
+        );
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({
+            ...action.products,
+            amount: 1,
+          });
+        }
+      });
     default:
-      return state || [];
+      return state;
   }
 }
